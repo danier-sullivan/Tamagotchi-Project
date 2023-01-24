bd760620d6ff4b0436b820e49c09728e278df6b9
DanGit0893 <dannysullivan300@gmail.com>
2023-01-24 16:48:59 -0600

    Created Tamagotchi class and instantiated test pet

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-alert("hello")
+class Tamagotchi {
+    constructor(){
+        this.name = "";
+        this.age = 0;
+        this.hunger = 10;
+        this.tiredness = 10;
+        this.boredom = 10;
+    }
+    eat(){
+        this.hunger++
+    }
+    play(){
+        this.boredom++
+    }
+    sleep(){
+        this.tiredness++
+    }
+    //Setname(){}
+}
+
+const testTama = new Tamagotchi();
+console.log(testTama)
