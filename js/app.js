15c60a3d841026fd50b2811c46da3d0b96a6149e
DanGit0893 <dannysullivan300@gmail.com>
2023-01-24 19:31:06 -0600

    set up buttons in js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-class Tamagotchi {
-    constructor(){
-        this.name = "";
+//Create 
+class Saiyan {
+    constructor(name){
+        this.name = name;
         this.age = 0;
         this.hunger = 10;
         this.tiredness = 10;
@@ -18,5 +19,17 @@ class Tamagotchi {
     //Setname(){}
 }
 
-const testTama = new Tamagotchi();
+const testTama = new Saiyan();
 console.log(testTama)
+
+//set up buttons for later use
+const startButton = document.querySelector(`#Start`);
+const newSaiyan = new Saiyan ("");
+const feedButton = document.querySelector('#Feed');
+const trainButton = document.querySelector('#Train');
+const restButton = document.querySelector('#Sleep');
+
+class Game {
+
+    
+}
